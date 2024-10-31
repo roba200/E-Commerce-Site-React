3be73d436931a986b81e1782453675c9e5d489f7
Roba <pasindudeemantha2000@gmail.com>
2024-10-31 11:14:07 +0530

    finish login page + add red button

---
+++ src/components/RedButton/RedButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RedButton.css'
+
+const Redbutton = ({text}) => {
+  return (
+    <button type="button" className='red-button'>{text}</button>
+   
+  );
+}
+
+export default Redbutton
+
--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Redbutton from "../../components/RedButton/Redbutton";
+import "./LogIn.css";
 
 function LogIn() {
   return (
-    <><Header></Header>
-    <Footer></Footer>
+    <>
+      <Header></Header>
+      <div className="login-container">
+        <img src="./Side Image.png" alt="Image" srcset="" />
+        <div className="login-wrapper">
+          <div className="login-form">
+            <div className="title">Log in to Exclusive</div>
+            <div className="subtitle">Enter your details below</div>
+            <input
+              type="text"
+              className="email"
+              placeholder="Email or Phone Number"
+            />
+            <br />
+            <input
+              type="password"
+              className="password"
+              placeholder="Password"
+            />
+            <div className="button-side">
+              <div className="login-button">
+                <Redbutton text="Log In" />
+              </div>
+              <div className="forgot-password">
+                Forgot Password?
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
     </>
-    
-  )
+  );
 }
 
-export default LogIn
+export default LogIn;
