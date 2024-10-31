d85d33e20b0a656ac00b29818dc538eb4650e686
Roba <pasindudeemantha2000@gmail.com>
2024-10-31 13:18:18 +0530

    finished signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LogIn from "./pages/LogIn/LogIn";
+import SignUp from "./pages/SignUP/SignUp";
 
 function App() {
   return (
-  <div className="App">
-    <LogIn></LogIn>
-  </div>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<LogIn/>}></Route>
+          <Route path="/signup" element={<SignUp/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <LogIn></LogIn> */}
+    </div>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <section className="h-wrapper">
-        <div className="h-container">
-            <img src="./logo.png" alt="logo" width={100}/>
-            <div className="h-menu">
-                <a href="">Home</a>
-                <a href="">Contact</a>
-                <a href="">About</a>
-                <a href="">Sign Up</a>
-            </div>
+      <div className="h-container">
+        <img src="./logo.png" alt="logo" width={100} />
 
-            <div className="search-input">
-            <input type="text" placeholder="What are you looking for?"/>
-            <img src="./mag-glass.png" alt="" className='magnifying-glass'/>
-            </div>
+        <div className="h-menu">
+          <a href="">Home</a>
+          <a href="">Contact</a>
+          <a href="">About</a>
+          <a href="" onClick={() => navigate("/signup")}>Sign Up</a>
         </div>
-        <div className="line"></div>
+
+        <div className="search-input">
+          <input type="text" placeholder="What are you looking for?" />
+          <img src="./mag-glass.png" alt="" className="magnifying-glass" />
+        </div>
+      </div>
+      <div className="line"></div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
-
+export default Header;
