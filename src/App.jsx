43cab56dbd1f097a2391f55ab7e3510ff071ffa0
Tharindu <senanananda123@gmail.com>
2024-11-06 07:38:06 +0530

    Create acount page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LogIn from "./pages/LogIn/LogIn";
 import SignUp from "./pages/SignUP/SignUp";
 import WishList from "./pages/WishList/WishList";
 import CheckOut from "./pages/CheckOut/CheckOut";
+import Account from "./pages/Account/Account";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
           <Route path="/signup" element={<SignUp/>}></Route>
           <Route path="/wishList" element={<WishList/>}/>
           <Route path="/checkout" element={<CheckOut/>}/>
+          <Route path="/account" element={<Account/>}/>
+
         </Routes>
       </BrowserRouter>
       {/* <LogIn></LogIn> */}
